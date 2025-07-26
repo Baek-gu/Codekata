d1d345dd686ec024c452378d4ebe1f0d264fe019
back9 <dofr1564@naver.com>
2025-07-26 12:41:36 +0900

    Codakata git 변경 및 파일 추가

---
+++ Codekata/부족한 금액 계산하기.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+
+    for (int i = 1; i <= count; i++)
+    {
+        answer += i * price; 
+    }
+
+    if (answer > money) 
+    {
+        answer -= money; 
+    }
+    else
+    {
+        return 0;
+    }
+
+    return answer;
+}
+
---
+++ Codekata/수박수박수박수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 1)
+        {
+            answer.append("��");
+        }
+        else
+        {
+            answer.append("��");
+        }
+    }
+
+    return answer;
+}
